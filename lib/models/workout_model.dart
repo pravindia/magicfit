b348909c02276c9924191218149af8868e0511b7
Pravin Kumar <pravinda4hero@gmail.com>
2022-11-25 10:52:32 +0530

    add workout

--- lib/models/workout_model.dart
+++ lib/models/workout_model.dart
@@ -1,1 +1,76 @@
+import 'dart:convert';
 
+import 'package:flutter/material.dart';
+import 'package:localstorage/localstorage.dart';
+
+import '../config/constants.dart';
+import '../config/mock_data.dart';
+import './serializers/workout.dart';
+import './serializers/workout_set.dart';
+
+class WorkoutModel {
+  final LocalStorage storage = LocalStorage(workoutStorageKey);
+
+  final List<Workout> _workout = [];
+  List<Workout> get getWorkoutList => _workout;
+
+  saveWorkout() async {
+    try {
+      final ready = await storage.ready;
+      if (ready) {
+        var json = _workout.map((e) => e.toJson()).toList();
+        var arr = jsonEncode(json);
+        await storage.setItem(workoutStorageKey, arr);
+      }
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  loadWorkout() async {
+    try {
+      final ready = await storage.ready;
+      if (ready) {
+        final json = await storage.getItem(workoutStorageKey);
+        var items;
+        if (json != null) items = jsonDecode(json);
+        if (items != null) {
+          for (var item in items) {
+            await addWorkout(Workout.fromJson(item));
+          }
+        }
+        if (_workout.isEmpty) {
+          for (var json in mockdata) {
+            await addWorkout(Workout.fromJson(json));
+          }
+        }
+      }
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  addWorkout(Workout w) async {
+    _workout.add(w);
+    await saveWorkout();
+  }
+
+  Future<Workout> addNewWorkout(String name) async {
+    int nextid = _workout
+        .reduce((value, element) => (value.id > element.id) ? value : element)
+        .id;
+    var newWorkout =
+        Workout(id: nextid, sets: [], name: name, date: DateTime.now());
+    await addWorkout(newWorkout);
+    return newWorkout;
+  }
+
+  addWorkoutSet(int i, WorkoutSet w) async {
+    Workout current = _workout.firstWhere((element) => element.id == i);
+    current.sets.add(w);
+    _workout.removeWhere((element) => element.id == current.id);
+    _workout.add(current);
+  }
+
+  deleteWorkoutSet() async {}
+}
--- lib/screens/workout_list.dart
+++ lib/screens/workout_list.dart
@@ -1,3 +1,4 @@
+import 'package:app/models/workout_model.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -15,53 +16,89 @@ class WorkoutList extends StatefulWidget {
 
 class _WorkoutListState extends State<WorkoutList> {
   List<Workout> workoutlist = [];
+  final WorkoutModel _model = WorkoutModel();
+
+  TextEditingController name = TextEditingController();
+
   @override
   void initState() {
-    loadData();
+    Future.delayed(Duration.zero, () async {
+      await loadData();
+    });
     super.initState();
   }
 
-  loadData() {
+  loadData() async {
     // mockdata
-    for (var json in mockdata) {
-      workoutlist.add(Workout.fromJson(json));
+    await _model.loadWorkout();
+    if (mounted) {
+      setState(() {
+        workoutlist = _model.getWorkoutList;
+      });
     }
   }
 
-  createnewDialog() {
-    showDialog(
-        context: context,
-        builder: (context) {
-          return Dialog(
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(40)),
-              elevation: 16,
-              child: Container(
-                padding: const EdgeInsets.all(30),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(
-                      'Add New workout',
-                      style: Theme.of(context).textTheme.bodyLarge,
-                    ),
-                    const TextField(
-                      key: addworkoutTextkey,
-                      autofocus: true,
-                    ),
-                    const SizedBox(height: 20),
-                    ElevatedButton(
-                      style: const ButtonStyle(
-                        padding: MaterialStatePropertyAll(
-                            EdgeInsets.symmetric(horizontal: 30, vertical: 10)),
-                      ),
-                      onPressed: () {},
-                      child: const Text('Create'),
-                    )
-                  ],
+  createWorkout() async {
+    // Validation
+    try {
+      if (name.text.isNotEmpty) {
+        Workout newWorkout = await _model.addNewWorkout(name.text);
+        if (mounted) {
+          setState(() {
+            workoutlist = _model.getWorkoutList;
+          });
+        }
+      } else {
+        //  Error Message
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text(e.toString()),
+      ));
+    }
+  }
+
+  createnewDialog() async {
+    await showDialog(
+      context: context,
+      builder: (_) {
+        return Dialog(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+          elevation: 16,
+          child: Container(
+            padding: const EdgeInsets.all(30),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  'Add New workout',
+                  style: Theme.of(context).textTheme.bodyLarge,
                 ),
-              ));
-        });
+                TextField(
+                  key: addworkoutTextkey,
+                  autofocus: true,
+                  controller: name,
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  style: const ButtonStyle(
+                    padding: MaterialStatePropertyAll(
+                        EdgeInsets.symmetric(horizontal: 30, vertical: 10)),
+                  ),
+                  onPressed: () async {
+                    FocusManager.instance.primaryFocus?.unfocus();
+                    await createWorkout();
+                    Navigator.of(context).maybePop();
+                  },
+                  child: const Text('Create'),
+                )
+              ],
+            ),
+          ),
+        );
+      },
+    );
   }
 
   String formatDat(DateTime? date) {
